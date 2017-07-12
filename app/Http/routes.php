1a280279ab0e78b66625409cc53577403a04ffbb
Jenn Dykes <jenndykes88@gmail.com>
2017-07-12 15:03:41 -0400

    new fitl laravel project

---
+++ app/Http/routes.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('welcome', function () {
+    return view('welcome');
+});
+Route::get('hello', function () {
+    return view('hello', ['name' => 'Jenn']);
+});
+Route::get('new', function () {
+    return view('hello', ['name' => 'Jennifer']);
+});
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+	<h1>Welcome to My Project, <?php echo $name; ?></h1>
+	<p>I hope you enjoy your stay.</p>
+</body>
+</html>
