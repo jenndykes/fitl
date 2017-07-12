0649f5176f04db3fa0359aa836f5953031381555
Jenn Dykes <jenndykes88@gmail.com>
2017-07-12 18:11:14 -0400

    added and removed a column in the events table

---
+++ database/migrations/2017_07_12_220125_add_test_to_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTestToEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+            $table->string('test');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+            $table->dropColumn('test');
+        });
+    }
+}
